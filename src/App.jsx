e9bd6f6cbdfd8f9d0c3238e72aaba4ae19173b52
Ali Arif <aliarif5787@gmail.com>
2025-11-17 13:05:53 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,21 +10,12 @@ import Testimonials from './components/Testimonials';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
 import Footer from './components/Footer';
-<<<<<<< HEAD
-import { useEffect } from 'react';
 import '@n8n/chat/style.css';
-import { createChat } from '@n8n/chat';
-=======
->>>>>>> fc8d9cd00782a615263fe055e87ceaaced48fcf4
+
 
 const App = () => {
  
-  useEffect(() => {
-		createChat({
-			webhookUrl: 'https://ahmadkhan526401.app.n8n.cloud/webhook/e104e40e-6134-4825-a6f0-8a646d882662/chat'
-		});
-	}, []);
- 
+
   return (
     <div className="min-h-screen bg-gradient-to-br">
       <ToastContainer/>
